c6dc79991b62d91a2b63a85b0cf104e4d9392455
Thiago.l <thiago.l17@yahoo.com>
2020-08-16 19:31:21 -0300

    function delete shedule-items

--- public/scripts/deleteField.js
+++ public/scripts/deleteField.js
@@ -1,8 +1,9 @@
 //delete field
 
 const schedule = document.querySelector("#schedule-items");
-const deleteField = document.querySelector(".schedule-item .delete-time");
 const fieldItem = document.querySelector(".schedule-item");
+const deleteField = document.querySelector(".delete-time");
+
 
     deleteField.addEventListener("click", () => {
         schedule.removeChild(fieldItem);
