9270b406820dafe9993af6b67bd19e78e79a1e2a
Thiago-l <thigo.l17@yahoo.com>
2020-10-08 17:17:32 -0300

    delete fields

--- public/scripts/deleteField.js
+++ public/scripts/deleteField.js
@@ -1,10 +1,8 @@
-//delete field
-
 const schedule = document.querySelector("#schedule-items");
 const fieldItem = document.querySelector(".schedule-item");
 const deleteField = document.querySelector(".delete-time");
 
+    deleteField.addEventListener('click', e => {
+        fieldItem.parentNode.removeChild(fieldItem);
 
-    deleteField.addEventListener("click", () => {
-        schedule.removeChild(fieldItem);
     });
